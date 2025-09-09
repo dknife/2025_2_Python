875197ce6e4c99dbddccf31402d32789695753c6
Dknife <young.min.kang@gmail.com>
2025-09-09 11:34:53 +0900

    Add files via upload

---
+++ Lectures/Lec01Ex/01_first.py
@@ -0,0 +1,9 @@
+print("hello world")
+
+a = 3
+b = 4
+
+빗변 = (a**2 + b**2)**0.5
+
+print(빗변)
+
---
+++ Lectures/Lec01Ex/04_print.py
@@ -0,0 +1,24 @@
+
+###### 프린트 함수 이해하기
+
+print("Hello world")
+
+print('hello world')
+
+print("Hello", "World")
+
+print("Hello" + "World")  # <== "Hello"+"World" = "HelloWorld"
+
+print(100)
+
+print("100")
+
+print(2+4)
+
+print("2+4")
+
+print("2"+"4")
+
+
+
+
