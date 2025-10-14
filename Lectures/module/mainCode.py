533b9c17a915e4ef81144c664a0848fe45dccd0d
Dknife <young.min.kang@gmail.com>
2025-10-14 10:29:52 +0900

    Add files via upload

---
+++ Lectures/module/mainCode.py
@@ -0,0 +1,18 @@
+
+import myFunctions
+import myClass
+
+
+var1 = 5
+var2 = 6
+
+result = myFunctions.add(var1,var2)
+
+print(result)
+
+dog1 = myClass.Dog('Harry', 3)
+dog2 = myClass.Dog('Louis', 4)
+
+
+dog1.bark()
+dog2.bark()
---
+++ Lectures/module/myClass.py
@@ -0,0 +1,12 @@
+
+class Dog :
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+
+    def bark(self) :
+        print(f'{self.age}세인 {self.name}가 짖어요.... 왕왕!')
+
+    
