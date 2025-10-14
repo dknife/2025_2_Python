2b01a36952219a3f7aaad90b62bff4b7ebb3a843
Dknife <young.min.kang@gmail.com>
2025-10-14 10:36:57 +0900

    Add files via upload

---
+++ Lectures/module/mainCode_alias.py
@@ -0,0 +1,18 @@
+
+import myFunctions as func
+import myClass as c
+
+
+var1 = 5
+var2 = 6
+
+result = func.add(var1,var2)
+
+print(result)
+
+dog1 = c.Dog('Harry', 3)
+dog2 = c.Dog('Louis', 4)
+
+
+dog1.bark()
+dog2.bark()
---
+++ Lectures/module/mainCode_fromImport.py
@@ -0,0 +1,18 @@
+
+from myFunctions import *
+from myClass import *
+
+
+var1 = 5
+var2 = 6
+
+result = add(var1,var2)
+
+print(result)
+
+dog1 = Dog('Harry', 3)
+dog2 = Dog('Louis', 4)
+
+
+dog1.bark()
+dog2.bark()
